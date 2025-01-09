f63e2340e2e2616d8e2744ab53bfbf01c9b1cd8f
Kirill <kirdro@yandex.ru>
2025-01-09 12:30:04 +0700

    autoscroll

---
+++ src/app/hooks/useScrollToBottom.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+const useScrollToBottom = (data: unknown) => {
+	const ref = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (ref.current) {
+			console.log(
+				'sdfasdfasdfasdf',
+				ref.current.scrollHeight,
+				ref.current.scrollTop,
+			);
+			// ref.current.scroll({ top: ref.current.scrollHeight });
+			// ref.current.scrollTop = ref.current.scrollHeight;
+			ref.current.scrollIntoView({ block: 'end' });
+		}
+
+		// window.scrollTo(0, 0);
+	}, [data]);
+
+	return ref;
+};
+
+export default useScrollToBottom;
